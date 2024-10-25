3ba0a280af388b640e974381ef668811646f3c0f
JCarlosJimenezC <JUAN.JIMENEZCASTRILLO@ucr.ac.cr>
2024-10-25 15:15:11 -0600

    JuanCarlos.java

---
+++ JuanCarlosJimenezCastrillo_C33980.java
@@ -0,0 +1,16 @@
+public class JuanCarlosJimenezCastrillo_C33980 {
+
+    
+
+ 
+        
+        public static void main(String[] args) {
+    
+    System.out.println("Hola broooo");
+    
+    }// fin del main
+     
+}
+
+
+
