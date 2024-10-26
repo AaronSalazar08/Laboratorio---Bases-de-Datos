4654d4a176bf01f95925fc29e24b841666d09180
barojas04 <barojas04@gmail.com>
2024-10-26 08:38:56 -0600

    Update BryanRojasZuniga_C16913.java

--- BryanRojasZuniga_C16913.java
+++ BryanRojasZuniga_C16913.java
@@ -3,6 +3,6 @@ import javax.swing.JOptionPane;
 public class BryanRojasZuniga_C16913 {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hola, este es el documento de Bryan Rojas :D "); 
+        JOptionPane.showMessageDialog(null, "Hola, este es el documento de Bryan Rojas, C16913 :D "); 
     }
 }
