1e2866e2a3c945ab6a65e6aeae432541fde2b991
1KATZUNO <147660938+1KATZUNO@users.noreply.github.com>
2024-10-28 18:33:22 -0600

    Mejora de Codigo (David)

---
+++ RafaelHernandez_C23740.java
@@ -0,0 +1,16 @@
+
+
+public class RafaelHernandez_C23740 {
+
+    public static void main(String[] args) {
+        System.out.println("Rafael David hernández González, C23740");
+
+        int edad=0;
+
+        for(int i=0; i<=20; i++){
+
+            edad=i;
+        }
+        System.out.println(edad);
+    }
+}
